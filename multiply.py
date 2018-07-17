03c88165e537473ac81416894a2a21cfce4a380d
Liam Goyette <lgoyette@bmt.local>
2018-07-17 17:36:04 -0400

    "The second of the greatest commits."

---
+++ multiply.py
@@ -0,0 +1,25 @@
+input1 = input("Enter numbers to multiply:")
+
+numProduct = 1
+for i in input1:
+    numProduct = numProduct * int(i)
+print (numProduct)
+
+"""def multiply(x, y, *varNum) :
+    numProduct = 1
+    for i in varNum:
+      if int(i) == int:
+        numProduct = numProduct * int(i) 
+    return int(x) * int(y) * numProduct
+    
+    x = inputFirst[:1]
+y = inputFirst[1:2]
+finProduct = []
+for i in inputFirst[2:]:
+    finProduct.append(i)
+lastVar = str(finProduct)
+print (lastVar)
+
+print (multiply(x,y,lastVar))
+
+ """
